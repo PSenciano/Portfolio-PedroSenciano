e0a35891c1718452298f498f846f158299ef68e9
Pedro Augusto Costa Senciano <50505259+PSenciano@users.noreply.github.com>
2024-04-02 17:12:55 -0300

    continuação do projeto, menu dos projetos funcional

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ const projects = [
         img: "assets/imgs/teste1.jpg",
         name: "Projeto 1",
         text: "PROJETO 1 description.",
+        tech: "Technologies 1",
     },
     {
         id: 1,
         img: "assets/imgs/teste2.jpg",
         name: "Projeto 2",
         text: "PROJETO 2 description.",
+        tech: "Technologies 2",
     },
     {
         id: 2,
         img: "assets/imgs/teste3.jpg",
         name: "Projeto 3",
         text: "PROJETO 3 description.",
+        tech: "Technologies 3",
     },   
 ]
 
@@ -25,6 +28,7 @@ const projects = [
 const img = document.querySelector("#img-projects");
 const projectsTitle = document.querySelector("#projects-title");
 const description = document.querySelector("#projects-description");
+const tech = document.querySelector('#technologies');
 
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
@@ -34,15 +38,16 @@ let currentItem = 0;
 
 //Load Initial Item
 window.addEventListener("DOMContentLoaded", function() {
-    showCharacter();
+    showProject();
 });
 
 //Show Project Function
-function showCharacter() {
+function showProject() {
     const item = projects[currentItem];
     img.src = item.img;
     projectsTitle.textContent = item.name;
     description.textContent = item.text;
+    tech.textContent = item.tech;
 }
 
 //Prev Button
@@ -51,7 +56,7 @@ prev.addEventListener('click', function() {
     if(currentItem < 0) {
         currentItem = projects.length - 1;
     }
-    showCharacter();
+    showProject();
 });
 
 //Next Button
@@ -60,5 +65,5 @@ next.addEventListener('click', function() {
     if(currentItem > projects.length - 1) {
         currentItem = 0;
     }
-    showCharacter();
+    showProject();
 });
