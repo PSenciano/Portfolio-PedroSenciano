27a9e042816201fc378023b108dc66d10b1507a4
Pedro Augusto Costa Senciano <50505259+PSenciano@users.noreply.github.com>
2024-04-19 17:09:46 -0300

    animações

--- js/script.js
+++ js/script.js
@@ -91,3 +91,56 @@ function closeMenu () {
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+
+
+
+
+
+
+
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting && entry.target.classList.contains('hidden')) {
+            entry.target.classList.add('show-animation')
+        } else {
+            entry.target.classList.remove('show-animation')
+        }
+
+
+        if(entry.isIntersecting && (entry.target.classList.contains('hidden-udy') || entry.target.classList.contains('hidden-duy'))) {
+            entry.target.classList.add('show-animation-y')
+        } else {
+            entry.target.classList.remove('show-animation-y')
+        }
+
+        if(entry.isIntersecting && (entry.target.classList.contains('hidden-lrx') || entry.target.classList.contains('hidden-rlx') )) {
+            entry.target.classList.add('show-animation-x')
+        }
+        else {
+            entry.target.classList.remove('show-animation-x')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden');
+const udyElements = document.querySelectorAll('.hidden-udy');
+const duyElements = document.querySelectorAll('.hidden-duy');
+const lrxElements = document.querySelectorAll('.hidden-lrx');
+const rlxElements = document.querySelectorAll('.hidden-rlx');
+
+
+elements.forEach( (element) => myObserver.observe(element));
+udyElements.forEach( (element) => myObserver.observe(element));
+duyElements.forEach( (element) => myObserver.observe(element));
+lrxElements.forEach( (element) => myObserver.observe(element));
+rlxElements.forEach( (element) => myObserver.observe(element));
+
+
+
+
+
+
+
+
